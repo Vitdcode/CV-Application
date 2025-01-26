b361c8d009cb97fcc9f881e1c8cd97d83921f160
Vitdcode <vitalijdietrich@gmail.com>
2025-01-27 00:31:06 +0100

    removed old css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
@@ -9,15 +8,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> ce9f59cdc9b97489d3b21c49f92c76ad163601b4
